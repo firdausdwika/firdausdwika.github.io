7481bab8b3dff895171e269022a04184d9645063
firdausdwika <firdausdwika@gmail.com>
2018-11-11 17:59:35 +0700

    add index.html

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $title = 'Firdaus Dwika';
+header('Location: https://firdausdwika.github.io');
 include dirname(__FILE__) . '/includes/header.php';
 	?>
 	<div class="h-100 row align-items-center main-content">
@@ -7,4 +8,5 @@ include dirname(__FILE__) . '/includes/header.php';
 include dirname(__FILE__) . '/includes/menubar.php';
 
 include dirname(__FILE__) . '/includes/footer.php';
+exit();
 ?>
