f139c836e4fc156927f5fcad7f01ab68ec9bb3c9
firdausdwika <firdausdwika@gmail.com>
2018-11-11 18:12:44 +0700

    add js

--- indexs.php
+++ indexs.php
@@ -1,6 +1,5 @@
 <?php
 $title = 'Firdaus Dwika';
-header('Location: https://firdausdwika.github.io');
 include dirname(__FILE__) . '/includes/header.php';
 	?>
 	<div class="h-100 row align-items-center main-content">
@@ -8,5 +7,4 @@ include dirname(__FILE__) . '/includes/header.php';
 include dirname(__FILE__) . '/includes/menubar.php';
 
 include dirname(__FILE__) . '/includes/footer.php';
-exit();
 ?>
